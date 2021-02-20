7eaa735174ba5f6ea99a5af667c8e5d4ce759a71
Dilara Derinpınar <dilaraderinpinar1@gmail.com>
2021-02-20 23:34:21 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            Console.WriteLine("************GET ALL LIST*********");
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine($"Id: {item.Id} \n Model: {item.Description} \n Model Year: {item.ModelYear} \n Color id: {item.ColorId} \n Daily Price: {item.DailyPrice} \n Brand ID: {item.BrandId}");
+            }
+
+            Console.WriteLine("***************");
+
+            Console.WriteLine("--GetById=2");
+            foreach (var item in carManager.GetById(2))
+            {
+                Console.WriteLine($"Car id: {item.Id} \nCar model: {item.Description}");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal:ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{BrandId = 1,ColorId = 1,DailyPrice = 200,Description = "Nissan Juke",Id=1,ModelYear = 2020},
+                new Car{BrandId = 2,ColorId = 1,DailyPrice = 100,Description = "Renault Captur",Id=2,ModelYear = 2017},
+                new Car{BrandId = 2,ColorId = 3,DailyPrice = 250,Description = "Nissan X-TRAIL",Id=3,ModelYear = 2021},
+                new Car{BrandId = 3,ColorId = 4,DailyPrice = 150,Description = "Ford Fiesta",Id=4,ModelYear = 2016},
+                new Car{BrandId = 4,ColorId = 1,DailyPrice = 120,Description = "Toyota Yaris",Id=5,ModelYear = 2017}
+            };
+        }
+        public  void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public  void Update(Car car)
+        {
+
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.BrandId = car.BrandId;
+
+
+        }
+
+        public  void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+
+        public  List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        
+
+        public  List<Car> GetById(int id)
+        {
+            return _cars.Where(c => c.Id == id).ToList();
+        }
+    }
+}
